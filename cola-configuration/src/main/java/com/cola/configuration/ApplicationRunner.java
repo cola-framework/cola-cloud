1697cf20a05b1751698f19f1c4b21947a84aae61
james-shijiachen <jiachen.shi@accenture.com>
2016-06-16 17:39:42 +0800

    add by james.shi 20160616

---
+++ cola-configuration/src/main/java/com/cola/configuration/ApplicationRunner.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2002-${Year} the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cola.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * cola
+ * Created by jiachen.shi on 6/16/2016.
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+
+}
