0365a9c1d7320c2b0636c91e96101a003519eb7f
Shijiachen <jiachen.shi@accenture.com>
2017-07-07 17:25:02 +0800

    update by james.shi 2017-07-07

--- platform/config/src/main/java/com/cola/platform/config/ConfigServerRunner.java
+++ platform/config/src/main/java/com/cola/platform/config/ConfigServerRunner.java
@@ -13,26 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cola.platform.registration;
+package com.cola.platform.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * cola
  * Created by jiachen.shi on 6/16/2016.
  */
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerRunner {
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerRunner {
 
-    private static Logger logger = LoggerFactory.getLogger(EurekaServerRunner.class);
+    private static Logger logger = LoggerFactory.getLogger(ConfigServerRunner.class);
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(EurekaServerRunner.class);
+        SpringApplication app = new SpringApplication(ConfigServerRunner.class);
         //app.setShowBanner(false);
         app.run(args);
     }
