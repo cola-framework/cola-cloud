5e4f741f86affc9367604834bda826debd86305f
jiachenshi <jiachen.shi@accenture.com>
2018-09-27 14:29:16 +0800

    update by james.shi 2018/09/10

--- service/order/src/main/java/com/cola/service/order/WebApplicationRunner.java
+++ service/order/src/main/java/com/cola/service/order/WebApplicationRunner.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
