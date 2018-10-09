cd19bfd9897b2be0bf2b231749ab036f6f412d17
jiachen.shi <jiachen.shi@accenture.com>
2018-10-09 16:26:05 +0800

    update by james.shi 2018/10/08

--- lib/jpa/src/main/java/com/cola/lib/jpa/entity/AbstractEntity.java
+++ lib/jpa/src/main/java/com/cola/lib/jpa/entity/AbstractEntity.java
@@ -15,19 +15,9 @@
  */
 package com.cola.lib.jpa.entity;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Locale;
-
-import javax.persistence.Cacheable;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Version;
 
 /**
  * cola
