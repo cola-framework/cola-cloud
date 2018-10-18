74bed903e2daed09b19030e7657ed7d78befe203
jiachen.shi <jiachen.shi@accenture.com>
2018-10-18 11:25:09 +0800

    update by james.shi 2018.10.17

--- lib/ruleengine/src/test/java/com/cola/lib/ruleengine/service/impl/RuleEngineServiceTest.java
+++ lib/ruleengine/src/test/java/com/cola/lib/ruleengine/service/impl/RuleEngineServiceTest.java
@@ -1,14 +1,14 @@
 package com.cola.lib.ruleengine.service.impl;
 
-import com.cola.lib.ruleengine.configruation.TestConfiguration;
-import com.cola.lib.ruleengine.entity.DroolsKIESession;
-import com.cola.lib.ruleengine.service.RuleEngineService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
+//import com.cola.lib.ruleengine.configruation.TestConfiguration;
+//import com.cola.lib.ruleengine.entity.DroolsKIESession;
+//import com.cola.lib.ruleengine.service.RuleEngineService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by jiachen.shi on 11/10/2016.
